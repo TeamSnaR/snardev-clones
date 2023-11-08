3be659892b598fd86bba52c173b779b1ce98cd26
LHAR <devlana@outlook.com>
2023-11-08 08:22:23 +0000

    Update app component title and add affected:xerogo script

--- apps/xerogo/src/app/app.component.ts
+++ apps/xerogo/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'snardev-clones-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'xerogo';
+  title = 'clones';
 }
