5f692ecb406e95f84dd7ee755cb177e1d89d1dd1
LHAR <devlana@outlook.com>
2023-12-13 18:01:51 +0000

    Added save* and remove* stub

--- libs/pmb/app-shell/src/lib/dashboard.service.ts
+++ libs/pmb/app-shell/src/lib/dashboard.service.ts
@@ -1,61 +1,79 @@
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { Budget } from '@snardev-clones/pmb/shared/models';
+import { EMPTY, Observable, of } from 'rxjs';
 
 @Injectable()
 export class DashboardService {
-  getBudget() {
-    return of({
-      id: '1',
-      date: new Date(2023, 10),
-      projected: {
-        amount: 100,
-        currency: 'GBP',
-      },
-      actual: {
-        amount: 100,
-        currency: 'GBP',
-      },
-      difference: {
-        amount: 0,
-        currency: 'GBP',
-      },
-      incomes: [
-        {
-          id: '1',
-          projected: {
-            amount: 100,
-            currency: 'GBP',
-          },
-          actual: {
-            amount: 100,
-            currency: 'GBP',
-          },
-          difference: {
-            amount: 20,
-            currency: 'GBP',
-          },
-          description: 'Salary',
+  readonly #budget = {
+    id: '1',
+    date: new Date(2023, 10),
+    projected: {
+      amount: 100,
+      currency: 'GBP',
+    },
+    actual: {
+      amount: 100,
+      currency: 'GBP',
+    },
+    difference: {
+      amount: 0,
+      currency: 'GBP',
+    },
+    incomes: [
+      {
+        id: '1',
+        projected: {
+          amount: 100,
+          currency: 'GBP',
         },
-      ],
-      expenses: [
-        {
-          id: '1',
-          category: 'Housing',
-          projected: {
-            amount: 50,
-            currency: 'GBP',
-          },
-          actual: {
-            amount: 50,
-            currency: 'GBP',
-          },
-          difference: {
-            amount: 0,
-            currency: 'GBP',
-          },
-          description: 'Rent',
+        actual: {
+          amount: 100,
+          currency: 'GBP',
         },
-      ],
-    });
+        difference: {
+          amount: 20,
+          currency: 'GBP',
+        },
+        description: 'Salary',
+      },
+    ],
+    expenses: [
+      {
+        id: '1',
+        category: 'Housing',
+        projected: {
+          amount: 50,
+          currency: 'GBP',
+        },
+        actual: {
+          amount: 50,
+          currency: 'GBP',
+        },
+        difference: {
+          amount: 0,
+          currency: 'GBP',
+        },
+        description: 'Rent',
+      },
+    ],
+  };
+  getBudget(): Observable<Budget> {
+    return of(this.#budget);
+  }
+
+  saveIncome(): Observable<void> {
+    return EMPTY;
+  }
+
+  removeIncome(): Observable<void> {
+    return EMPTY;
+  }
+
+  saveExpense(): Observable<void> {
+    return EMPTY;
+  }
+
+  removeExpense(): Observable<void> {
+    return EMPTY;
   }
 }
