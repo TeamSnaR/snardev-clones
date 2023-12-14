34cb5e62e102d80c20db130feca50f4c8651cc1a
LHAR <devlana@outlook.com>
2023-12-14 07:54:06 +0000

    integrated app with api

--- apps/mock-api/src/app/app.service.ts
+++ apps/mock-api/src/app/app.service.ts
@@ -2,7 +2,64 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  #budget = {
+    id: '1',
+    date: new Date(2023, 10),
+    projected: {
+      amount: 0,
+      currency: 'GBP',
+    },
+    actual: {
+      amount: 0,
+      currency: 'GBP',
+    },
+    difference: {
+      amount: 0,
+      currency: 'GBP',
+    },
+    incomes: [
+      {
+        id: '1',
+        projected: {
+          amount: 150,
+          currency: 'GBP',
+        },
+        actual: {
+          amount: 120,
+          currency: 'GBP',
+        },
+        difference: {
+          amount: 20,
+          currency: 'GBP',
+        },
+        description: 'Salary',
+      },
+    ],
+    expenses: [
+      {
+        id: '1',
+        category: 'Housing',
+        projected: {
+          amount: 50,
+          currency: 'GBP',
+        },
+        actual: {
+          amount: 50,
+          currency: 'GBP',
+        },
+        difference: {
+          amount: 0,
+          currency: 'GBP',
+        },
+        description: 'Rent',
+      },
+    ],
+  };
   getData(): { message: string } {
     return { message: 'Hello API' };
+  }
+
+  getBudget() {
+    return this.#budget;
   }
 }
--- libs/pmb/app-shell/src/lib/dashboard.service.ts
+++ libs/pmb/app-shell/src/lib/dashboard.service.ts
@@ -1,64 +1,13 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 import { Budget } from '@snardev-clones/pmb/shared/models';
-import { EMPTY, Observable, of } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 
 @Injectable()
 export class DashboardService {
-  readonly #budget = {
-    id: '1',
-    date: new Date(2023, 10),
-    projected: {
-      amount: 100,
-      currency: 'GBP',
-    },
-    actual: {
-      amount: 100,
-      currency: 'GBP',
-    },
-    difference: {
-      amount: 0,
-      currency: 'GBP',
-    },
-    incomes: [
-      {
-        id: '1',
-        projected: {
-          amount: 100,
-          currency: 'GBP',
-        },
-        actual: {
-          amount: 100,
-          currency: 'GBP',
-        },
-        difference: {
-          amount: 20,
-          currency: 'GBP',
-        },
-        description: 'Salary',
-      },
-    ],
-    expenses: [
-      {
-        id: '1',
-        category: 'Housing',
-        projected: {
-          amount: 50,
-          currency: 'GBP',
-        },
-        actual: {
-          amount: 50,
-          currency: 'GBP',
-        },
-        difference: {
-          amount: 0,
-          currency: 'GBP',
-        },
-        description: 'Rent',
-      },
-    ],
-  };
+  httpClient = inject(HttpClient);
   getBudget(): Observable<Budget> {
-    return of(this.#budget);
+    return this.httpClient.get<Budget>('api/budget');
   }
 
   saveIncome(): Observable<void> {
